e9a6e6c168ecfb76a3242dea53d417789086907d
Glauton Santos <glauton.santos_external@clark.de>
2022-01-20 13:52:45 -0300

    Refatorando

--- server.js
+++ server.js
@@ -61,18 +61,6 @@ function selectUser(profile) {
 }
 
 app.get('/', (req, res) => {
-    const desjejum = {
-        bebidas: [ '0x1F964','Bebidas', 'Café', 'Leite Quente / Frio (Contém Lactose)', 'Leite de Soja', 'Suco de Maracujá' ],
-        paes: [ '0x1F35E',' Pães','Pão Carioca (Contém Glúten)' ,'Pão Sovado (Contém Glúten)' ],
-        frutas: ['0x1F34E',' Frutas', 'Laranja', 'Melão Espanhol'],
-        especial: ['0x1F31F',' Especial', 'Achocolatado (Contém Lactose)', 'Bolo (Contém Lactose e Glúten)']
-    }
-    const restaurants =  {
-        pici1: ['Pici I', '100','15'],
-        pici2: ['Pici II', '100', '45'],
-        benfica: ['Benfica', '100', '60'],
-        porangabucu: ['Porangabuçu', '100', '90']
-    }
     res.render('views/index', { desjejum: desjejum, restaurants: restaurants });
 });
 
